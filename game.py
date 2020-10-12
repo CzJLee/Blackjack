55dcb7e09d6b16761381383e8619d907c524f3e9
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-10-12 15:17:05 -0700

    Make Blackjack Class

--- game.py
+++ game.py
@@ -2,32 +2,42 @@ import pydealer
 
 from player import Player, Dealer
 
-# Create dealer and player class
-dealer = Dealer()
-player = Player()
+class Blackjack:
+	def __init__(self, shoe_size = 1):
+		self.dealer = Dealer("Dealer")
+		self.shoe_size = shoe_size
+		self.deck = self.new_deck()
 
-# Create and shuffle deck of shoe size 1
-deck = pydealer.Deck()
-deck.shuffle()
+	def shuffle_deck_in_place(self):
+		self.deck.shuffle()
 
-def deal_cards(dealer, player):
-	dealer.hand = deck.deal(2)
-	player.hand = deck.deal(2)
+	def new_deck(self):
+		shoe = pydealer.Stack()
 
-def check_natural(dealer, player):
-	# Return a list of players with natural 21
-	naturals = []
+		for i in range(self.shoe_size):
+			shoe.add(pydealer.Deck())
+		
+		shoe.shuffle()
 
-	# Check if dealer has natural 21
-	if dealer.has_natural():
-		naturals.append(dealer)
-	
-	# Check if player has natural 21
-	if player.has_natural():
-		naturals.append(player)
+		return shoe
 
-	return naturals
+	def deal_cards(self, player):
+		self.dealer.hand = self.deck.deal(2)
+		player.hand = self.deck.deal(2)
 
-def hit(player):
-	player.hand += deck.deal(1)
+	def check_natural(self, player):
+		# Return a list of players with natural 21
+		naturals = []
 
+		# Check if dealer has natural 21
+		if self.dealer.has_natural():
+			naturals.append(self.dealer)
+		
+		# Check if player has natural 21
+		if player.has_natural():
+			naturals.append(player)
+
+		return naturals
+
+	def hit(self, player):
+		player.hand += self.deck.deal(1)
